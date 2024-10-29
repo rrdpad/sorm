99e5166bfb8ad27c09f12aa95b66d06faa6dea04
rrdpad <ptichka.denozavra05@mail.ru>
2024-10-29 23:39:38 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
 use rand::Rng;
 use rodio::{source::Source, Decoder, OutputStream};
-use std::fs::{self, File, OpenOptions};
+use std::fs::{self, File};
 use std::io::{self, BufReader};
+use std::path::PathBuf;
 use std::process::exit;
-use std::{env, thread};
+use std::thread;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    path: String,
+
+    #[arg(default_value_t = 60)]
+    seconds: u32,
+
+    #[arg(long, default_value_t = false)]
+    log: bool,
+}
 
 fn main() {
-    let argv: Vec<String> = env::args().collect();
-    if argv.len() == 1 {
-        print!("type path as argument...");
-        exit(0);
-    }
-    let mut seconds: u32 = match argv.len() != 3 {
-        true => 60,
-        false => match argv[2].trim().parse::<u32>() {
-            Ok(r) => r,
-            Err(_) => 60,
-        },
-    };
+    let args = Args::parse();
+
+    let seconds = args.seconds;
+
     println!("{}", seconds);
-    let read_dir = match fs::read_dir(&argv[1]) {
+    let read_dir = match fs::read_dir(&args.path) {
         Ok(d) => d,
         Err(_) => {
             print!("path not found");
             exit(0)
         }
     };
-    let mut lists: Vec<String> = Vec::new();
+    let mut lists: Vec<PathBuf> = Vec::new();
     for music in read_dir {
-        let asd = music
-            .unwrap()
-            .path()
-            .as_os_str()
-            .to_str()
-            .unwrap()
-            .to_string();
-        if asd.ends_with(".mp3") {
-            lists.push(asd)
+        let music = music.unwrap().path();
+
+        if music.to_str().unwrap().to_string().ends_with(".mp3") {
+            lists.push(music)
         }
     }
     if lists.len() == 0 {
@@ -51,7 +52,12 @@ fn main() {
         let source = Decoder::new(file).unwrap();
         let r = rand::thread_rng().gen_range(1..=seconds);
         let _ = stream_handle.play_raw(source.convert_samples());
-        println!("playing {}", &lists[rand_sound].to_string());
+        if args.log {
+            println!(
+                "playing {}",
+                &lists[rand_sound].as_os_str().to_str().unwrap().to_string()
+            );
+        }
 
         std::thread::sleep(std::time::Duration::from_secs(r as u64))
     });
